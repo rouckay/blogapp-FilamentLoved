8f6c27547c374729830ff957c42bb0dc4a83fea2
rouckay <farhadwardak2018@gmail.com>
2024-07-18 10:10:27 +0430

    View is update single Post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,12 @@
-<x-app-layout meta-description="Developer is the best one" >
+<x-app-layout meta-description="Developer is the best one">
 
     <section class="w-full md:w-2/3 flex flex-col items-center px-3">
-        
-            @foreach ($posts as $post)
-            <x-post-item :post="$post" >
+        @foreach ($posts as $post)
+            <x-post-item :post="$post">
+            </x-post-item>
+        @endforeach
+        {{ $posts->links() }}
+    </section>
 
-            </x-post-item> 
-                
-            @endforeach
-            {{ $posts->links() }}
-        </section>
-
-        <x-sidebar/>
+    <x-sidebar />
 </x-app-layout>
